8b2535d323fa37a1d8c7ccf5e8506dc40dff1b9d
faisal-din <dahrifaisal56@gmail.com>
2026-02-22 21:52:48 +0500

    Implement unique username check

---
+++ app/api/check-username-unique/route.ts
@@ -0,0 +1,76 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import { usernameValidation } from "@/Schemas/signUpSchema";
+import z from "zod";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const queryParams = {
+      username: searchParams.get("username"),
+    };
+
+    // Validate the query parameters using Zod
+    const validationResult = UsernameQuerySchema.safeParse(queryParams);
+
+    if (!validationResult.success) {
+      const usernameErrors =
+        validationResult.error.format().username?._errors || [];
+
+      console.error("Validation errors for username:", usernameErrors);
+
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid username",
+          errors:
+            usernameErrors?.length > 0
+              ? usernameErrors?.join(", ")
+              : ["Invalid username format"],
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = validationResult.data;
+
+    const existingUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        { status: 400 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is unique",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error checking username uniqueness:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username uniqueness",
+      },
+      { status: 500 }
+    );
+  }
+}
