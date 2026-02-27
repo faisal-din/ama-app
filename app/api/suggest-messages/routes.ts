dd92acadc86eac17f16980147233e80cae353c51
faisal-din <dahrifaisal56@gmail.com>
2026-02-27 23:01:01 +0500

    Integrate Open AI feature

---
+++ app/api/suggest-messages/routes.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import { streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export const runtime = "edge";
+
+export async function POST() {
+  try {
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    const result = await streamText({
+      model: openai("gpt-4.1-mini"),
+      prompt,
+    });
+
+    return result.toTextStreamResponse();
+  } catch (error) {
+    console.error("OpenAI API error:", error);
+
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    } else {
+      console.error("An unexpected error occurred:", error);
+      throw error;
+    }
+  }
+}
