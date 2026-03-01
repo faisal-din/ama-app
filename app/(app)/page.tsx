b0fe70b64924a9d552945f220ac939e7eb8506f0
faisal-din <dahrifaisal56@gmail.com>
2026-03-01 15:20:23 +0500

    Implement sin-in with AuthJS and

---
+++ app/(app)/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <>
+      {/* Main content */}
+      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            Dive into the World of Anonymous Feedback
+          </h1>
+          <p className="mt-3 md:mt-4 text-base md:text-lg">
+            True Feedback - Where your identity remains a secret.
+          </p>
+        </section>
+      </main>
+
+      {/* Footer */}
+      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+        © 2026 True Feedback. All rights reserved.
+      </footer>
+    </>
+  );
+}
